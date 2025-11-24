d4f7fe5e3c09ed708a282689fe05534ee329ab72
danielvasic <dvasic1@gmail.com>
2025-11-24 15:18:49 +0100

    Predavanja (JWT) 24.11.2025. - Dodan autentikacijski mehanizan za pocetak rada s JWT tokenom.

---
+++ src/main/java/ba/sum/fpmoz/filmotekabaza/auth/JWTUtil.java
@@ -0,0 +1,53 @@
+package ba.sum.fpmoz.filmotekabaza.auth;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    public SecretKey getSigningKey(){
+        return new SecretKeySpec(
+                secret.getBytes(),
+                "HmacSHA256"
+        );
+    }
+
+    public String generateToken(String username) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiration);
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public boolean validateToken(String token){
+        try {
+            Jwts.parser()
+                    .verifyWith(getSigningKey())
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
